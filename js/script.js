7524b76c12c2e0ec12a3c0c80482a37a66c6c36c
Misha Evangelista <89716798+b3stg1rlm1sh4@users.noreply.github.com>
2021-08-30 05:20:47 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,137 @@
+const scoreDisplay = document.getElementById("score-display");
+const timeDisplay = document.getElementById("time-display");
+const blocks = [
+  document.getElementById("btn1"), document.getElementById("btn2"), document.getElementById("btn3"), document.getElementById("btn4"), document.getElementById("btn5"), document.getElementById("btn6"), document.getElementById("btn7"), document.getElementById("btn8"), document.getElementById("btn9"), document.getElementById("btn10"),
+  document.getElementById("btn11"), document.getElementById("btn12"),
+  document.getElementById("btn13"), document.getElementById("btn14"),
+  document.getElementById("btn15"), document.getElementById("btn16")
+]
+
+let used = [];
+let hira = ["あ", "え", "い", "お", "う", "か", "け", "き"];
+let kata = ["ア", "エ", "イ", "オ", "ウ", "カ", "ケ", "九"];
+let colours = [
+  "#3df58a", "#f53d3d", "#f5d63d", "#f5b13d",
+  "#3ddcf5", "#3d6bf5", "#b83df5", "#f53dd6"
+]
+let bCount = 1;
+let colour;
+let pair = 1;
+let time = 30;
+let score = 0;
+let over = false;
+
+function beenUsed(val, arr) {
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] == val) return true;
+  }
+  return false;
+}
+
+blocks.forEach(function(block) {
+  let _;
+  let index;
+
+  if(bCount <= 8) {
+    while(true) {
+      index = Math.floor(Math.random() * 8);
+      _ = hira[index];
+      if(beenUsed(_, used)) continue;
+      else break;
+    }
+    used[bCount] = _;
+  }else {
+    while(true) {
+      index = Math.floor(Math.random() * 8);
+      _ = kata[index];
+      if(beenUsed(_, used)) continue;
+      else break;
+    }
+    used[bCount] = _;
+  }
+
+  bCount++;
+  block.textContent = _;
+  block.setAttribute("hirakata", hira[index]);
+  console.log(`${block.id}: ${block.getAttribute("hirakata")}`);
+
+  block.addEventListener("click", function match() {
+    pair++;
+    block.style.backgroundColor = colour;
+    block.style.color = "#ffffff";
+  })
+})
+
+const colourCycle = setInterval(() => {
+  switch(pair) {
+    case 1:
+      colour = colours[0];
+      break;
+    case 3:
+      colour = colours[1];
+      break;
+    case 5:
+      colour = colours[2];
+      break;
+    case 7:
+      colour = colours[3];
+      break;
+    case 9:
+      colour = colours[4];
+      break;
+    case 11:
+      colour = colours[5];
+      break;
+    case 13:
+      colour = colours[6];
+      break;
+    case 15:
+      colour = colours[7];
+      break;
+    case 17:
+      over = true;
+      break;
+  }
+}, 15)
+
+const timeProg = setInterval(() => {
+  if(time >= 1) time -= 1;
+}, 1000)
+
+const updater = setInterval(() => {
+  if(time == 0) {
+    over = true;
+  }
+  timeDisplay.textContent = `Time: ${time}`;
+}, 5)
+
+const endDetector = setInterval(() => {
+  if(over) {
+    clearInterval(colourCycle);
+    clearInterval(timeProg);
+    clearInterval(updater);
+
+    let matched = [
+      "", "", "", "", "", "", "", "",
+      "", "", "", "", "", "", "", ""
+    ];
+
+    blocks.forEach(function(block) {
+      block.disabled = true;
+
+      for(let i = 0; i < blocks.length; i++) {
+        if(blocks[i].style.backgroundColor == "") continue;
+        if(block.id == blocks[i].id) continue;
+        if(beenUsed(block.id, matched)) continue;
+        if(block.getAttribute("hirakata") != blocks[i].getAttribute("hirakata")) continue;
+        if(block.style.backgroundColor != blocks[i].style.backgroundColor) continue;
+        matched[i] = blocks[i].id;
+        score++;
+        console.log(`${block.id} : ${blocks[i].id}`);
+      }
+    })
+
+    scoreDisplay.textContent = `Score: ${score}`;
+    clearInterval(endDetector);
+  }
+}, 100)
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Hirakata | b3stg1rlm1sh4
+## What is Hirakata?
+Hirakata is a symbol matching game with a twist. Hiragana symbols are to be matched with their corresponding katakana symbols. To play simply identify a symbol within the 4x4 grid and click it, followed by it's corresponding symbol. Should your time expire, or no more pairs can be matched, your score will be evaluated and displayed
+## Where to play it?
+The game should operate fine within any browser(as of 30/08/2021), simply `clone` the repo and utilise vscode's live server extension. Alternatively, (provided [repl.it](https://replit.com) doesn't cease to exist) the game may be played [here](https://Hirakata.b3stg1rlm1sh4.repl.co)
+## Contributors
++ *Misha Evangelista*
+  + GitHub: [b3stg1rlm1sh4](https://github.com/b3stg1rlm1sh4)
